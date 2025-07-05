32ed7f3bdeb5588e78849d92244979b19fe14ea8
Rohith <rohith.m94@gmail.com>
2025-07-05 10:37:22 +0000

    image addition

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,14 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+
+      <div>
+        <img src="https://reactnexus.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fzoho.743e5bb8.png&w=256&q=75" >
+
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Skywalker says ! count is {count}
         </button>
        
       </div>
