c394e4fa3ecdf56257ea9933a89afaf93965ed47
Rohith <rohith.m94@gmail.com>
2025-07-05 10:38:51 +0000

    syntax fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h1>Vite + React</h1>
 
       <div>
-        <img src="https://reactnexus.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fzoho.743e5bb8.png&w=256&q=75" >
+        <img src="https://reactnexus.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fzoho.743e5bb8.png&w=256&q=75" />
 
       </div>
       <div className="card">
