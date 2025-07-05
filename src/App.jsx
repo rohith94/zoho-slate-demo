514a5e93d354c94a8da87a31177debdb6ab4a750
Rohith <rohith.m94@gmail.com>
2025-07-05 10:44:48 +0000

    meme addition

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,15 @@ function App() {
       <h1>Vite + React</h1>
 
       <div>
+
+        <img
+        src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWNuaWZmcHdsbXQ5NzVqZW45NGZsaXJxczRjb3p1eXlnaGNnbmtzdiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HUkOv6BNWc1HO/giphy.gif"
+        alt="Victory Meme"
+        className="rounded-lg w-full max-w-sm"
+      />
+      </div>
+
+      <div>
         <img src="https://reactnexus.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fzoho.743e5bb8.png&w=256&q=75" />
 
       </div>
